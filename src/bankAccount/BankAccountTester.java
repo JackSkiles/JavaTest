6d4f28ec12dcef1c692d5e89bd019437288ca024
Jack Skiles <jackskiles420@gmail.com>
2020-08-24 17:02:23 -0400

    created new test project

---
+++ src/bankAccount/BankAccountTester.java
@@ -0,0 +1,13 @@
+package bankAccount;
+
+
+public class BankAccountTester{
+    /** Tests the methods of the BankAccount class. 8 @param args not used 9 */
+    public static void main(String[] args){
+        BankAccount harrysChecking = new BankAccount();
+        harrysChecking.deposit(1000);
+        harrysChecking.withdraw(500);
+        System.out.println(harrysChecking.getBalance());
+        System.out.println("Expected: 1500");
+    }
+}
